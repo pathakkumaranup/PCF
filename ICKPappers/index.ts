7d8bc8d6b597c098ec5f3a518ffee41f9a783f10
Thomas Miller <tmillerdeloitte@azhasystems.onmicrosoft.com>
2023-05-14 18:14:25 +0530

    First Commit

--- ICKPappers/index.ts
+++ ICKPappers/index.ts
@@ -16,9 +16,9 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
     // Datalist element.
     private datalistElement: HTMLDataListElement;
 
-    private _siret: string;
-    private _companyName: string;
-    private _codeNAF: string;
+    private _phoneNumber: string;
+    private _name: string;
+    private _email: string;
     private _libelleCodeNAF: string;
     private _formeJuridique: string;
     private _adresse: string;
@@ -29,14 +29,14 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
 
     }
 
-	/**
-	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-	 * Data-set values are not initialized here, use updateView.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
-	 */
+    /**
+     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+     * Data-set values are not initialized here, use updateView.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+     * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+     */
     public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
         this.localNotifyOutputChanged = notifyOutputChanged;
         this.context = context;
@@ -66,7 +66,7 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
 
         this.inputElement = document.createElement("input");
         this.inputElement.name = "autocomplete_" + this.id
-        this.inputElement.placeholder = "Search Companies Database...";
+        this.inputElement.placeholder = "Search Contact Database...";
         this.inputElement.autocomplete = "off";
         this.inputElement.className = "ms-SearchBox-field"
         this.inputElement.setAttribute("list", "list_" + this.id);
@@ -103,25 +103,25 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
     }
 
 
-	/**
-	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-	 */
+    /**
+     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+     */
     public updateView(context: ComponentFramework.Context<IInputs>): void {
-        ((this._companyName != undefined) ? this.inputElement.value = this._companyName : null)
+        ((this._name != undefined) ? this.inputElement.value = this._name : null)
     }
 
     public clearFields(evt: Event) {
         console.log("Clear Fields")
 
-        this._companyName = ""
-        this._siret = ""
-        this._codeNAF = ""
-        this._libelleCodeNAF = ""
-        this._formeJuridique = ""
-        this._adresse = ""
-        this._code_postal = ""
-        this._ville = ""
+        this._name = ""
+        this._email = ""
+        this._phoneNumber = ""
+        // this._libelleCodeNAF = ""
+        // this._formeJuridique = ""
+        // this._adresse = ""
+        // this._code_postal = ""
+        // this._ville = ""
         this.localNotifyOutputChanged();
     }
 
@@ -129,86 +129,71 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
 
         // Connect to Suggestion Pappers API and get the suggestions as the user presses keys and update dropdown.
         let input = (this.inputElement.value as any) as string;
-        let key = "SIRET";
+        let key = "Name";
         if (input.indexOf(key) == -1 && input.length > 0) {
-            this.datalistElement.innerHTML = "";            
-            let query = "v2?q=" + encodeURIComponent(input)+ "&longueur=5";
-            let options = {
-                host: 'suggestions.pappers.fr/',
-                path: query
-            }
-            const https = require('https');
 
-            https.get(options, (resp: any) => {
-                let data = '';
-                // A chunk of data has been recieved.
-                resp.on('data', (chunk: any) => {
-                    data += chunk;
-                });
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                    var response = JSON.parse(data);
-                    console.log(response);
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL29yZzAzOWMwM2E0LmFwaS5jcm0uZHluYW1pY3MuY29tIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvM2FiNjkxYjYtODJlMS00Y2M3LWJkMDctNzc5MWQ3Njk1M2U2LyIsImlhdCI6MTY4NDA2MTgzMiwibmJmIjoxNjg0MDYxODMyLCJleHAiOjE2ODQwNjcyMjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUE0MlpObXl5bENrc0FJd1N3MmN1TUU4YU5jSTZXNDYyUytUTU9ZOEZmdDBScjEyZXdETzZMVFk5OXJRQk1nM3g0IiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6IjUxZjgxNDg5LTEyZWUtNGE5ZS1hYWFlLWEyNTkxZjQ1OTg3ZCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiR3VwdGEiLCJnaXZlbl9uYW1lIjoiU3VtaXQiLCJpcGFkZHIiOiIyMC4xNjkuMjEuMzEiLCJuYW1lIjoiU3VtaXQgR3VwdGEiLCJvaWQiOiIyMmFjYjg5MC00YmI3LTRiMGUtYTJlZi1hNzE0ZGVjMGViMjciLCJwdWlkIjoiMTAwMzIwMDI5MUVBMEFFRCIsInJoIjoiMC5BVVlBdHBHMk91R0N4MHk5QjNlUjEybFQ1Z2NBQUFBQUFBQUF3QUFBQUFBQUFBQ0FBUEUuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoiemx3UFJRUldQSllrdFlHb2hQSVI1SG8xY211UHV4c3RzdExDQ2xoSno3VSIsInRpZCI6IjNhYjY5MWI2LTgyZTEtNGNjNy1iZDA3LTc3OTFkNzY5NTNlNiIsInVuaXF1ZV9uYW1lIjoiU3VtaXRAU3VtaXRiMS5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiJTdW1pdEBTdW1pdGIxLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6IkNwUHRMZHhvdEVDM1lESm1xTkVqQUEiLCJ2ZXIiOiIxLjAifQ.lsEKvKR7ZnmzqWfs-D3AjrtGyqxeDeJ9O9w0ruoF6b0XkYsEm3RcZXlWgEvvaFQL_18xrzFdVD1k1VcHzLJiUiT8GMqXH_t0_QsO5Exd_zwYose7IgAPxqft2uZMIsLrGaUhMGEGDv-O9_ug9Dw6p147vnQ14d5AtrDummmI3XfJqRaJy5fXDy3LrloMh6MvyOlKcXmIV3JD_6h1aBnbIeBiH91G8msyNErsv0OlZc6Ds1Nc4q-Z6NYgjqYmgK8xFx-t-OodTz7pqvux5Xnuo8G4gZ1Q6A5MiFNPWyWUt9lRPQcQr0Z7p7sf-nzLnUqdpLUGT5W67RQz3PR4i5qvGg");
+            myHeaders.append("Cookie", "ARRAffinity=2204d179a18aa70fdd7a3f8e3d215ffa368974248d810e0e10fa27bbca026abe15134d20c556b0b34b9b6ae43ec3f5dcdad61788de889ffc592af7aca85fc1c508DB544AFA6BEB47852420112; ReqClientId=e65f7a53-ca8c-4404-b656-8d915a321283; orgId=146c95ae-0ad4-ed11-aece-000d3a3ac637");
+            var raw = JSON.stringify({
+                "search": encodeURIComponent(input) + "~",
+                "usefuzzy": true,
+                "entities": [
+                    "contact"
+                ]
+            });
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw
+            };
+
+            fetch("https://org039c03a4.api.crm.dynamics.com/api/search/v1.0/query?entities=[\"contact\"]", requestOptions)
+                .then(response => response.text())
+                .then(result => {
+                    var respBody = JSON.parse(result);
+                    console.log(respBody);
                     var optionsHTML = "";
                     var optionsHTMLArray = new Array();
-                    for (var i = 0; i < response.resultats_nom_entreprise.length; i++) {
+                    for (var i = 0; i < respBody.value.length; i++) {
                         optionsHTMLArray.push('<option value="');
-                        optionsHTMLArray.push(response.resultats_nom_entreprise[i].denomination + ' (' + response.resultats_nom_entreprise[i].siege.code_postal + ' ' + response.resultats_nom_entreprise[i].siege.ville + ')' + ' - SIRET:' + response.resultats_nom_entreprise[i].siege.siret);
-                        optionsHTMLArray.push('"> ' + response.resultats_nom_entreprise[i].domaine_activite+ '</option>');                        
+                        optionsHTMLArray.push('Name:' + respBody.value[i].fullname + ", Phone:" + respBody.value[i].telephone1 + ", Email:" + respBody.value[i].emailaddress1);
+                        optionsHTMLArray.push('"> ' + respBody.value[i].fullname + '</option>');
                     }
                     this.datalistElement.innerHTML = optionsHTMLArray.join("");
                     this.localNotifyOutputChanged
-                });
+                })
+                .catch(error => console.log('error', error));
 
-            }).on("error", (err: { message: string; }) => {
-                console.log("Error: " + err.message);
-            });
         }
         else {
             console.log("in else");
+            console.log(this.inputElement.value);
             this.getDetails(this.inputElement.value)
         }
     }
 
     getDetails(value: string) {
-        let key = "SIRET"
+        let key = "Name"
         if (value.indexOf(key) > -1) {
+            var response = value.split(",");
             let _siret = value.substring(value.indexOf(key) + 6, value.length);
-            let query = "v2?q=" + _siret + "&cibles=siret";
-            let options = {
-                host: 'suggestions.pappers.fr/',
-                path: query
-            }
-            
-            const https = require('https');
 
-            https.get(options, (resp: any) => {
-                let data = '';
 
-                // A chunk of data has been recieved.
-                resp.on('data', (chunk: any) => {
-                    data += chunk;
-                });
+            console.log(response)
 
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                    var response = JSON.parse(data);
-                    console.log(response)
-                    this._siret = response.resultats_siret[0].siege.siret_formate
-                    this._companyName = response.resultats_siret[0].denomination
-                    this._codeNAF = response.resultats_siret[0].code_naf
-                    this._libelleCodeNAF = response.resultats_siret[0].libelle_code_naf
-                    this._formeJuridique = response.resultats_siret[0].forme_juridique
-                    this._adresse = response.resultats_siret[0].siege.adresse_ligne_1
-                    this._code_postal = response.resultats_siret[0].siege.code_postal
-                    this._ville = response.resultats_siret[0].siege.ville
-                    this.localNotifyOutputChanged();
+            this._name = response[0].substring(response[0].indexOf(':') + 1, response[0].length)
+            this._phoneNumber = response[1].substring(response[1].indexOf(':') + 1, response[1].length)
+            this._email = response[2].substring(response[2].indexOf(':') + 1, response[2].length)
+            // this._libelleCodeNAF = response.resultats_siret[0].libelle_code_naf
+            // this._formeJuridique = response.resultats_siret[0].forme_juridique
+            // this._adresse = response.resultats_siret[0].siege.adresse_ligne_1
+            // this._code_postal = response.resultats_siret[0].siege.code_postal
+            // this._ville = response.resultats_siret[0].siege.ville
+            this.localNotifyOutputChanged();
 
-                });
-
-            }).on("error", (err: { message: string; }) => {
-                console.log("Error: " + err.message);
-            });
         }
         else {
             return {};
@@ -216,29 +201,29 @@ export class SIRENEAutoComplete implements ComponentFramework.StandardControl<II
 
     }
 
-	/** 
-	 * It is called by the framework prior to a control receiving new data. 
-	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
-	 */
+    /** 
+     * It is called by the framework prior to a control receiving new data. 
+     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+     */
     public getOutputs(): IOutputs {
 
         return {
-            value: this._companyName,
-            siret: this._siret,
-            companyName: this._companyName,
-            codeNAF: this._codeNAF,
-            libelleCodeNAF: this._libelleCodeNAF,
-            formeJuridique: this._formeJuridique,
-            adresse: this._adresse,
-            code_postal: this._code_postal,
-            ville: this._ville,
+            value: this._name,
+            phoneNumber: this._phoneNumber,
+            name: this._name,
+            email: this._email,
+            // libelleCodeNAF: this._libelleCodeNAF,
+            // formeJuridique: this._formeJuridique,
+            // adresse: this._adresse,
+            // code_postal: this._code_postal,
+            // ville: this._ville,
         };
     }
 
-	/** 
-	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-	 * i.e. cancelling any pending remote calls, removing listeners, etc.
-	 */
+    /** 
+     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+     * i.e. cancelling any pending remote calls, removing listeners, etc.
+     */
     public destroy(): void {
         // Add code to cleanup control if necessary
     }
